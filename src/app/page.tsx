b8502a223a6026936041ab6380b9ca4b18f83420
lntori <lntori@deladetech>
2026-03-03 20:12:46 +0000

    under mobile view, i have added tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
   // Mobile: tabbed layout (PDF tab | Canvas tab)
   if (isMobile) {
     return (
-      <main className="flex flex-col h-screen bg-white overflow-hidden">
+      <main className="flex flex-col h-[95vh] bg-white overflow-hidden">
         <div className="flex shrink-0 border-b border-gray-200 bg-gray-50">
           <button
             type="button"
